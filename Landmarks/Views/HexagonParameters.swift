302e0b624bbd68f3563a2165f12aa5f69e744e9c
Robles Montanari <robles.png@gmail.com>
2022-05-04 10:12:50 -0300

    Added badge background

--- Landmarks/Views/HexagonParameters.swift
+++ Landmarks/Views/HexagonParameters.swift
@@ -13,9 +13,8 @@ struct HexagonParameters {
         let curve: CGPoint
         let control: CGPoint
     }
-    
+
     static let adjustment: CGFloat = 0.085
-    
     static let segments = [
         Segment(
             line:    CGPoint(x: 0.60, y: 0.05),
@@ -28,9 +27,9 @@ struct HexagonParameters {
             control: CGPoint(x: 0.00, y: 0.25 + adjustment)
         ),
         Segment(
-            line:    CGPoint(x: 0.00, y: 0.70 + adjustment),
-            curve:   CGPoint(x: 0.05, y: 0.80 + adjustment),
-            control: CGPoint(x: 0.00, y: 0.75 + adjustment)
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
         ),
         Segment(
             line:    CGPoint(x: 0.40, y: 0.95),
@@ -38,9 +37,9 @@ struct HexagonParameters {
             control: CGPoint(x: 0.50, y: 1.00)
         ),
         Segment(
-            line:    CGPoint(x: 0.95, y: 0.80 + adjustment),
-            curve:   CGPoint(x: 1.00, y: 0.70 + adjustment),
-            control: CGPoint(x: 1.00, y: 0.75 + adjustment)
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
         ),
         Segment(
             line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
