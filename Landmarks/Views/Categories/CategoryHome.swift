4e0c602c61b406e3f841e1711be5b8dee5edda72
Robles Montanari <robles.png@gmail.com>
2022-05-06 16:38:34 -0300

    Added a Category view

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Robles Montanari on 06/05/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, world!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
